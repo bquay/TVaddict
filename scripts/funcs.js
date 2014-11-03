5490d042ba77dc0d544004701271b3a8839dfd77
bquay <baq4@Pitt.edu>
2014-11-03 17:34:52 -0500

    changes

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -1,11 +1,17 @@
-function upvote(form){
+function upvote(form, comId){
 	var updownInput = document.getElementById('updown');
 	updownInput.value = "1";
+	var comStr = '' + comId
+	var comInput = document.getElementById(comStr);
+	comInput.value = "1";
 	form.submit();
 }
 
-function downvote(form){
+function downvote(form, comId){
 	var updownInput = document.getElementById('updown');
 	updownInput.value = "-1";
+	var comStr = '' + comId
+	var comInput = document.getElementById(comStr);
+	comInput.value = "-1";
 	form.submit();
 }
