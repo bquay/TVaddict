99869af741083c387ca44e9c8fc2999495cb0000
bquay <baq4@Pitt.edu>
2014-11-04 20:17:55 -0500

    disable button after clicking

--- scripts/funcs.js
+++ scripts/funcs.js
@@ -1,4 +1,6 @@
 function upvote(form, comId){
+	var buttonId = "upvote" + comId;
+	document.getElementById(buttonId).disabled = true;
 	var updownInput = document.getElementById('updown');
 	updownInput.value = "1";
 	var comInput = document.getElementById('comId');
@@ -7,6 +9,8 @@ function upvote(form, comId){
 }
 
 function downvote(form, comId){
+	var buttonId = "downvote" + comId;
+	document.getElementById(buttonId).disabled = true;
 	var updownInput = document.getElementById('updown');
 	updownInput.value = "-1";
 	var comInput = document.getElementById('comId');
