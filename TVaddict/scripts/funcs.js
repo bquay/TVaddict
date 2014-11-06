9aa6a7b7eca6a03b25582b12c356728d6664b272
samdlinn <shmueld770@hotmail.com>
2014-11-06 14:03:01 -0500

    Commiting as a test

---
+++ TVaddict/scripts/funcs.js
@@ -0,0 +1,23 @@
+function upvote(form, comId){
+	var buttonId = "upvote" + comId;
+	document.getElementById(buttonId).disabled = true;
+	var updownInput = document.getElementById('updown');
+	updownInput.value = "1";
+	var comInput = document.getElementById('comId');
+	comInput.value = comId;
+	form.submit();
+}
+
+function downvote(form, comId){
+	var buttonId = "downvote" + comId;
+	document.getElementById(buttonId).disabled = true;
+	var updownInput = document.getElementById('updown');
+	updownInput.value = "-1";
+	var comInput = document.getElementById('comId');
+	comInput.value = comId;
+	form.submit();
+}
+
+function disableClick(){
+	document.getElementById("submitId").disabled = true;
+}
