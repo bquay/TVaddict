a2447f2da225ddd0003f4f37445f8042674aac38
Brendan Quay <brendan.quay@gmail.com>
2015-04-16 19:31:18 -0400

    organize

--- autotest/Compile_Trend_Test.bat
+++
@@ -1,3 +0,0 @@
-javac -classpath "./selenium-java-2.45.0.jar;./libs/*" Gmail.java SeleniumGui.java
-@echo off
-Pause
--- autotest/Run_Trend_Test.bat
+++
@@ -1,3 +0,0 @@
-java -classpath ".;selenium-java-2.45.0.jar;./libs/*" SeleniumGui
-@echo off
-Pause
